f3ff43849061896f52bcb3e18e84e4c1f8d5d292
jarrebear <battistini.jarrett@gmail.com>
2026-09-10 20:49:56 +0000

    Modified laser reader to keep array of pointers of last 10 scans

--- laser_reader/include/laser_reader/laser_reader.hpp
+++ laser_reader/include/laser_reader/laser_reader.hpp
@@ -10,8 +10,9 @@ private:
 
   void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr laser_msg);
 
-  
-  float *last_laser_ranges;
+  //   float *last_laser_ranges;
+  float *last_ten_scans[10];
+  size_t scan_count_ = 0;
   /* DEFINE THE POINTER TO THE LASER RANGES ARRAY HERE */
 
 public:
--- laser_reader/src/laser_reader.cpp
+++ laser_reader/src/laser_reader.cpp
@@ -17,24 +17,54 @@ LaserReaderClass::LaserReaderClass() : Node("laser_reader") {
   rclcpp::sleep_for(2s);
 
   // Allocate an array of 50 floats
-  last_laser_ranges = new float[50];
+  //   last_laser_ranges = new float[50];
+  for (size_t i = 0; i <= 9; i++) {
+    last_ten_scans[i] = new float[50];
+  }
 }
 
 // Destructor
 /* CREATE THE CLASS DESTRUCTOR HERE */
 
-LaserReaderClass::~LaserReaderClass() { delete[] last_laser_ranges; }
+LaserReaderClass::~LaserReaderClass() {
+  // delete[] last_laser_ranges;
+  for (size_t i = 0; i <= 9; i++) {
+    delete[] last_ten_scans[i];
+  }
+}
 
 void LaserReaderClass::laser_callback(
     const sensor_msgs::msg::LaserScan::SharedPtr laser_msg) {
 
-  for (size_t i = 0; i < laser_msg->ranges.size() && i < 50; i++) {
-    last_laser_ranges[i] = laser_msg->ranges[i];
-  }
-  std::cout << "Number of ranges: " << laser_msg->ranges.size() << "\n";
+  // Need to copy oldest scan to assign to scan 0
+  float *oldest_scan_buffer = last_ten_scans[9];
 
-  std::cout << "Distance measured by the ray at the front of the robot: "
-            << last_laser_ranges[25] << "\n";
+  // shift scan i into i+1 starting at scan 8
+  for (size_t i = 9; i > 0; i--) {
+    last_ten_scans[i] = last_ten_scans[i - 1];
+  }
+
+  // re-assign pointer to previous scan 9 to scan 0
+  last_ten_scans[0] = oldest_scan_buffer;
+
+  // Assign new scan data to our most recent scan (0 is always newest data, 9
+  // oldest)
+
+  for (size_t i = 0; i < laser_msg->ranges.size() && i < 50; i++) {
+    last_ten_scans[0][i] = laser_msg->ranges[i];
+  }
+
+  //   std::cout << "Number of ranges: " << laser_msg->ranges.size() << "\n";
+  if (scan_count_ >= 10) {
+
+    std::cout
+        << "Range measured by the ray at the front for the last 10 scans: \n";
+
+    for (size_t i = 0; i <= 9; i++)
+      std::cout << "Scan " << i << ": " << last_ten_scans[i][25] << "\n";
+  }
+
+  scan_count_++;
 }
 
 int main(int argc, char **argv) {
